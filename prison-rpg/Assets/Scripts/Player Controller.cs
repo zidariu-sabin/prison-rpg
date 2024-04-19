0317547bbcbb9ea582afd335cd2a9d2cb93d035e
zidariu-sabin <zidariu19@gmail.com>
2024-04-19 10:28:04 +0300

    code-cleaning

--- prison-rpg/Assets/Scripts/Player Controller.cs
+++ prison-rpg/Assets/Scripts/Player Controller.cs
@@ -8,56 +8,48 @@ using UnityEngine.InputSystem;
 public class NewBehaviourScript : MonoBehaviour
 {
     Rigidbody rigidbody;
-    private float speed=5;
-    private Vector2 move;
-    private Vector2 clickPos;
-    private Transform m_transform;
+    private float _speed=5;
+    private Vector2 _move;
+    private Vector2 _clickPos;
+    private Transform _mtransform;
 
-    //Reads the inputted move
+    //Reads the inputted _move
     public void OnMove(InputAction.CallbackContext context)
-    {   //move is a vector 2 for wasd inputs
-        move = context.ReadValue<Vector2>();
-      //  Debug.Log(context.ReadValue<Vector2>());
+    {   //_move is a vector 2 for wasd inputs
+        _move = context.ReadValue<Vector2>();
     }
 
     public void OnStrike(InputAction.CallbackContext context)
     {   if(context.performed)
         {
-            clickPos = context.ReadValue<Vector2>();
-            Debug.Log(clickPos);
+            _clickPos = context.ReadValue<Vector2>();
+            Debug.Log(_clickPos);
         }
     }
 
-    private Camera viewCamera;
+    private Camera _viewCamera;
     void Start()
     {
-        m_transform = this.transform;
+        _mtransform = this.transform;
         //  rigidbody = GetComponent<Rigidbody>();
-          viewCamera = Camera.main;
+          _viewCamera = Camera.main;
     }
     
     void Update()
     {  
-        movePlayer();
-        pointerRotation();
+        MovePlayer();
+        PointerRotation();
     }
 
-    public void movePlayer()
+    public void MovePlayer()
     {   
-        //so character doesn't snap back to original place
-        if (move.sqrMagnitude > 0.1f)
-        {   //movement will assign move values to a vector
-            Vector3 movement = new Vector3(move.x, 0f, move.y);
-             
-            //rotate to look towards movement point with speed 0.15
-          //  transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
-            transform.Translate(Time.deltaTime * movement * speed, Space.World);
-        }
+            Vector3 movement = new Vector3(_move.x, 0f, _move.y);
+            transform.Translate(Time.deltaTime * movement * _speed, Space.World);
     }
 
-    public void pointerRotation()
+    public void PointerRotation()
     {
-        Vector3 mousePos = viewCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,viewCamera.transform.position.y));
+        Vector3 mousePos = _viewCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,_viewCamera.transform.position.y));
         transform.LookAt(mousePos + Vector3.up*transform.position.y);
     }
 }
