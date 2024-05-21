58e5bc1d47581bbcdf02f0597a47ed286f66c22e
dragosJJJ <dragos_juganariu@yahoo.com>
2024-05-22 01:27:17 +0300

    ITEM4
    -fixed collisions with objects and the jittering problem when colliding with them
    -changed player movement from transform to rigidBody type so now physics are applied

--- prison-rpg/Assets/Scripts/Player Controller.cs
+++ prison-rpg/Assets/Scripts/Player Controller.cs
@@ -7,14 +7,19 @@ using UnityEngine.InputSystem;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    Rigidbody rigidbody;
-    private float _speed=5;
+    bool moving;
+    public Rigidbody rb;
     private Vector2 _move;
-    private float _dash;
     private Vector2 _clickPos;
+
+    private float _speed=5;
+    public float maxSpeed = 10f;
+    public float decelerationTime = 1f;
+
+    private float _dash;
     private Transform _mtransform;
-    
-    
+    float forceMultiplier = 7f;
+
 
     //Reads the inputted _move
     public void OnMove(InputAction.CallbackContext context)
@@ -43,28 +48,42 @@ public class NewBehaviourScript : MonoBehaviour
     void Start()
     {
         _mtransform = this.transform;
-        //  rigidbody = GetComponent<Rigidbody>();
-          _viewCamera = Camera.main;
+        rb = GetComponent<Rigidbody>();
+        _viewCamera = Camera.main;
     }
     
     void Update()
-    {  
-        MovePlayer();
+    {
         PointerRotation();
+    }
+
+    void FixedUpdate()
+    {
+        MovePlayer();
     }
 
     public void MovePlayer()
     {
-            Vector3 movement = new Vector3(_move.x, 0f, _move.y);
-            
-            transform.Translate(CalculateMovementDistance(movement), Space.World);
-            if (_dash == 1)
-            {   
-                Vector3 startPosition = transform.position;
-                Vector3 desiredPosition = startPosition + CalculateMovementDistance(movement*40);
-                transform.position = Vector3.Lerp(startPosition, desiredPosition, 1f);
-                _dash = 0;
+        Vector3 movement = new Vector3(_move.x, 0f, _move.y);
+        if (_move.x != 0f || _move.y != 0f)
+        {
+            if (rb.velocity.magnitude < maxSpeed)
+            {
+                rb.velocity = movement * forceMultiplier;
             }
+        }
+        else
+        {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+        if (_dash == 1)
+        {   
+            Vector3 startPosition = transform.position;
+            Vector3 desiredPosition = startPosition + CalculateMovementDistance(movement*40);
+            transform.position = Vector3.Lerp(startPosition, desiredPosition, 1f);
+            _dash = 0;
+        }
     }
 
     public void PointerRotation()
